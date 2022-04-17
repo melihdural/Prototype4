be46feb26c5c29cfdd0368fbccc99291f69badeb
Melih Dural <duralmelih@gmail.com>
2022-04-18 00:47:13 +0300

    * Added powerup feature for player, *Spawn conditions set for powerup system, *Adjusted the behavior of the enemy object when it collides with the player while the powerup system is active

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float playerSpeed;
 
+    [SerializeField]
+    private GameObject powerupIndicator;
+    
     private Rigidbody playerRb;
 
     private GameObject focalPoint;
+
+    private float powerUpStrength = 15.0f;
+
+    [HideInInspector]
+    public bool hasPowerUp;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -23,5 +33,41 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * playerSpeed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDownRoutine());
+        }
+    }
+
+    IEnumerator PowerUpCountDownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("The player collide with: " + collision.gameObject.name + "with power up set to: " + hasPowerUp);
+        }
+    }
+    
+    
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,13 +6,18 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject enemyPrefab;
-
+    
+    [SerializeField]
+    private GameObject powerupIcon;
+    
     private float spawnRange = 9;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         RandomSpawn();
+        StartCoroutine(SpawnPowerupIcon());
     }
 
     // Update is called once per frame
@@ -28,6 +33,17 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosY);
 
         Instantiate(enemyPrefab, randomPos, Quaternion.identity);
+        
+    }
+    
+    public IEnumerator SpawnPowerupIcon()
+    {
+        yield return new WaitForSeconds(10);
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosY = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0.5f, spawnPosY);
+
+        Instantiate(powerupIcon, randomPos, Quaternion.identity);
     }
     
 }
